83e194e3b04c0c21c0360e2e2d10169ff1cc1f46
Flowsqy <Flo504.Dev@gmail.com>
2021-04-03 14:42:34 +0200

    Remove useless message

--- src/main/java/fr/flowsqy/componentreplacer/ComponentReplacerPlugin.java
+++ src/main/java/fr/flowsqy/componentreplacer/ComponentReplacerPlugin.java
@@ -2,12 +2,6 @@ package fr.flowsqy.componentreplacer;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Level;
-
 public class ComponentReplacerPlugin extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        getLogger().log(Level.INFO, "ComponentReplacer API correctly loaded");
-    }
 }
